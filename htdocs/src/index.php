50a9df013217304cb9212f4355fda81fe107553c
yife.greenspa <yife.greenspa@gmail.com>
2012-06-14 19:31:43 +0900

    トップページに新着動画を表示する機能をつけた
    最大10件まで

--- htdocs/src/index.php
+++ htdocs/src/index.php
@@ -1,14 +1,31 @@
 <?php
 
-require_once('../../libs/MySmarty.class.php');
+//データベースへの接続を開始
+$dbh = new PDO('mysql:host=localhost;dbname=nicolive', 'viewer', '8C80K53S3XTAzBDz0e');
+$dbh->query("SET NAMES utf8;");
 
+//Smartyライブラリ
+require_once('../../libs/MySmarty.class.php');
 $smarty = new MySmarty();
 
+//ログイン処理用
+require_once '../../libs/classes/access.class.php';
+$user = new flexibleAccess();
 
-$scalar = 'Hello my new smarty!';
-$sex = array('m'=>'おとこ', 'f'=>'おんな');
+    
+//トップページだけは未ログインでも見られるようにする
 
-$smarty->assign('scalar', $scalar);
-$smarty->assign('sex', $sex);
+//新着順にソートしてDBから全件とってくる
+$stmt = 'SELECT * FROM searchable_videos ORDER BY video_number desc limit 10;';
+$sth = $dbh->prepare($stmt);
+$sth->execute();
+$video_data = $sth->fetchAll();
 
+$smarty->assign('video_data', $video_data);
+    
 $smarty->display('index.html');
+
+
+
+//データベースへの接続を終了
+$dbh = null;
